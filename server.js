c7c507be66f5c36dc6d263e1f7d5e1cc6545fece
ronit <ronitsharma3870@gmail.com>
2023-09-23 18:19:50 +0530

    combined mongodb

--- server.js
+++ server.js
@@ -3,24 +3,31 @@ const app=Express();
 const bodyParser=require("body-parser");
 const {MongoClient}=require("mongodb");
 const Cors=require("cors");
-const UserconnectDB = require("./MongoDB/userDBConnect");
-const ProductconnectDB = require("./MongoDB/ProductDBConnect");
 app.use(Cors({origin:"http://localhost:3000",credentials:true}));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-// app.get("/logInData",async (req,res)=>{
-//     const db=await connectDB;
-//     const collection=db.collection("User-info");
-//     const f=collection.find();
-//     const arr=await f.toArray();
-//     res.json(arr);
-// })
+const uri="mongodb+srv://ronitsharma3870:ashu6608@cluster0.rtstlsp.mongodb.net/?retryWrites=true&w=majority";
+const client=new MongoClient(uri);
+let UserconnectDB,ProductconnectDB;
+async function Connect(){
+    try{
+        await client.connect();
+         UserconnectDB= client.db("Users");
+        console.log("Connceted to UserDB server!");
+         ProductconnectDB= client.db("fruits");
+        console.log("Connceted to ProductDB server!");
+        }
+        catch(e){
+            console.log("!!error connecting to MongoDB!!");
+        }
+       
+}
+Connect();
 
 app.post("/loginVerification",async (req,res)=>{
     const {Username,Password}=req.body;
-    const db=await UserconnectDB;
-    const collection=db.collection("User-info");
+    const collection=UserconnectDB.collection("User-info");
     const f=collection.find();
     const arr=await f.toArray();
     var c=0;
@@ -37,8 +44,7 @@ app.post("/loginVerification",async (req,res)=>{
 
 app.post("/Signup",async (req,res)=>{
     const {_id,FullName,email,Password}=req.body;
-    const db=await UserconnectDB;
-    const collection=db.collection("User-info");
+    const collection=UserconnectDB.collection("User-info");
     const f=collection.find();
     const arr=await f.toArray();
     var x=0;
@@ -56,8 +62,7 @@ app.post("/Signup",async (req,res)=>{
 });
 
 app.get("/productData",async (req,res)=>{
-    const db=await ProductconnectDB;
-    const collection=db.collection("cart");
+    const collection=ProductconnectDB.collection("cart");
     const f=collection.find();
     const arr=await f.toArray();
     res.json(arr);
