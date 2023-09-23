491c5c2470573ffa0a8ca0be8e06053c86e23e6b
Ronit_sharma004 <118843577+ronit044@users.noreply.github.com>
2023-09-23 19:31:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app=Express();
 const bodyParser=require("body-parser");
 const {MongoClient}=require("mongodb");
 const Cors=require("cors");
-app.use(Cors({origin:"http://localhost:3000",credentials:true}));
+app.use(Cors({origin:'*',credentials:true}));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
